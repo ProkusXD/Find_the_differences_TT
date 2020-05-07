c83d2fccbb1bd9612eac4460c292c7341026c7d5
Prokus <Prokopchuk_xD@Hotmail.com>
2020-05-07 20:32:05 +0300

    Реалізована логіка: - Поява хрестика при не попаданні по зоні відмінностей; - Створення зірочок в залежності від кількості зон відмінностей на сцені; - Заміна зображень з закритих зірочок на відкритті взалежності від кількості відгаданих зон; - Реалізовано програш і виграш (пока що у виглядді повідомлення в консоль).

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("LevelSetting")]
+    [Header("HealthBar")]
+    [Range(0,10)]public int maxPlayerHealth = 3;
+
     public List<RectTransform> diffAreas;
     public RectTransform differcesAreaDiffImg;
+    public RectTransform fallImg;
 
+    public int NowPlayerHealth { get; set; }
+
+    private int countAllCrntDiffArea = 0;
+    private int countNowFoundDiffArea = 0;  
+
+    public bool IsLose { get; private set; } = false;
+    public bool IsWin { get; private set; } = false;
     public static GameManager Instance { get; private set; }
 
     private void Awake()
@@ -16,22 +28,35 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         FindAllDiffAreas();
-        DublicateDiffArea();
     }
     private void Update()
     {
+        if (IsLose || IsWin)
+            return;
+
         if (Input.GetMouseButtonDown(0))
-        ReycastCheck();
+            ReycastCheck();
     }
 
     private void FindAllDiffAreas()
     {
-        GameObject[] allDiffAreas = GameObject.FindGameObjectsWithTag("DiffArea");
+        DifferencesArea[] allDiffAreas = FindObjectsOfType<DifferencesArea>();
+
+        if (allDiffAreas.Length == 0)
+        {
+            Debug.LogError("Зони відмінностей не знайденні на сцені");
+            return;
+        }
+
+        countAllCrntDiffArea = allDiffAreas.Length;
+        StarBar.Instance.Spawn(countAllCrntDiffArea);
 
         for (int i = 0; i < allDiffAreas.Length; i++)
         {
             diffAreas.Add(allDiffAreas[i].GetComponent<RectTransform>());
         }
+
+        DublicateDiffArea();
     }
     private void DublicateDiffArea()
     {
@@ -39,10 +64,10 @@ public class GameManager : MonoBehaviour
         {
             for (int i = 0; i < diffAreas.Count; i++)
             {
-                RectTransform dublicateArea = Instantiate(diffAreas[i]);
-                dublicateArea.SetParent(differcesAreaDiffImg);
-                dublicateArea.localPosition = diffAreas[i].localPosition;
-                dublicateArea.localScale = diffAreas[i].localScale;
+                RectTransform dublicateArea = Instantiate(diffAreas[i],differcesAreaDiffImg);
+
+                diffAreas[i].GetComponent<DifferencesArea>().CloneThisDiffArea = dublicateArea.gameObject;
+                dublicateArea.GetComponent<DifferencesArea>().CloneThisDiffArea = diffAreas[i].gameObject;
             }
         }
     }
@@ -53,19 +78,51 @@ public class GameManager : MonoBehaviour
 
         if (hit.collider != null)
         {
-            if (hit.collider.CompareTag("DiffArea"))
-                Correct();
-            else if (hit.collider.CompareTag("Image"))
-                Fall();
+            switch (hit.collider.tag)
+            {
+                case "DiffArea":
+                    Correct(hit.collider.GetComponent<DifferencesArea>());
+                    break;
+                case "Image":
+                    Fall();
+                    SpawnFallIng( hit.point);
+                    break;
+            }
         }
     }
 
+    private void SpawnFallIng(Vector2 pos)
+    {
+        fallImg.position = pos;
+        fallImg.GetComponent<Animator>().SetTrigger("Start");
+    }
+    private void Correct(DifferencesArea difArea)
+    {
+        if (IsWin || IsLose)
+            return;
+
+        difArea.Correct();
+        countNowFoundDiffArea++;
+        StarBar.Instance.OpenStar(countNowFoundDiffArea);
+        if (countNowFoundDiffArea == countAllCrntDiffArea)
+            Win();
+    }
     private void Fall()
     {
-        print("Fall");
+        if (IsLose || IsWin)
+            return;
+        HealthBar.Instance.TakeDamage(1);
+        if (NowPlayerHealth <= 0)
+            Lose();
     }
-    private void Correct()
+    private void Win()
     {
-        print("Correct");
+        IsWin = true;
+        Debug.Log("Win!");
+    }
+    private void Lose()
+    {
+        IsLose = true;
+        Debug.Log("Lose!");
     }
 }
