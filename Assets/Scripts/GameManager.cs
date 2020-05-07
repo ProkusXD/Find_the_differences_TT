2a6c4a2bc8e6c41282373a631676c97adfb9570d
Prokus <Prokopchuk_xD@Hotmail.com>
2020-05-07 15:08:01 +0300

    Створений макет рівння, тестова логіка доблювання зон відмінностей мож зображеннями і тестова логіка перевірки через рейкаст чи попали ми в зону або в інше місце на одному із фото

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<RectTransform> diffAreas;
+    public RectTransform differcesAreaDiffImg;
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        FindAllDiffAreas();
+        DublicateDiffArea();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        ReycastCheck();
+    }
+
+    private void FindAllDiffAreas()
+    {
+        GameObject[] allDiffAreas = GameObject.FindGameObjectsWithTag("DiffArea");
+
+        for (int i = 0; i < allDiffAreas.Length; i++)
+        {
+            diffAreas.Add(allDiffAreas[i].GetComponent<RectTransform>());
+        }
+    }
+    private void DublicateDiffArea()
+    {
+        if (diffAreas != null)
+        {
+            for (int i = 0; i < diffAreas.Count; i++)
+            {
+                RectTransform dublicateArea = Instantiate(diffAreas[i]);
+                dublicateArea.SetParent(differcesAreaDiffImg);
+                dublicateArea.localPosition = diffAreas[i].localPosition;
+                dublicateArea.localScale = diffAreas[i].localScale;
+            }
+        }
+    }
+    private void ReycastCheck()
+    {
+        Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(touchPos, Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("DiffArea"))
+                Correct();
+            else if (hit.collider.CompareTag("Image"))
+                Fall();
+        }
+    }
+
+    private void Fall()
+    {
+        print("Fall");
+    }
+    private void Correct()
+    {
+        print("Correct");
+    }
+}
